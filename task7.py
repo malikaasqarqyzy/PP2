4e00883f4ecff61183a52173f3bcf0b50fc3b9ab
malikaasqarqyzy <127949003+malikaasqarqyzy@users.noreply.github.com>
2023-09-16 16:27:03 +0600

    Add files via upload

---
+++ task7.py
@@ -0,0 +1,10 @@
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+
+p1 = (n1//2)+(n1%2)
+p2 = (n2//2)+(n2%2)
+p3 = (n3//2)+(n3%2)
+
+s = p1+p2+p3
+print(s)
---
+++ task8.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+l = int(input())
+n = int(input())
+sum = 2*l + a*(2*n - 1) + 2*b*((n-1))
+print(sum)
